15c0e5f8caf961b26352d17494d3db622cd5441a
alumno <alumno@LAB00PC00>
2019-04-11 20:36:33 -0300

    BuscarIndiceLibreEnArray

--- Clases/main.c
+++ Clases/main.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "utn.h"
-
 #define LEN_LISTA    100
+
+int buscarIndiceLibreEnArray(char lista[][20],int tamanio,int *pIndice);// va a multiplicar el indice por 56
+
 int main()
 {
 
@@ -29,6 +31,9 @@ int main()
         nombres[i][0] = '\0';
     }
 
+    //HARCODEO DE PRIMER LUGAR Y SEGUNDO LUGAR
+    nombres[0][0] ='A';
+    nombres[1][0] ='T';
 
     //
     while(1)
@@ -44,18 +49,49 @@ int main()
         {
             case 1:
             {
+                int posLibre;
                 printf("agregar nombre\n");
+                if(buscarIndiceLibreEnArray(nombres,LEN_LISTA,&posLibre)==0)
+                {
+                    printf("se encontro lugar en %d\n",posLibre);
 
+                }
+                scanf("%s",nombres[i]);
+                i++;
                 break;
+            }
+            case 2:
+            {
+                for(i=0;i<LEN_LISTA;i++)
+                {
+                    printf("Nombre cargado : %s",nombres[i]);
+                    break;
+                }
             }
         }
 
     }
 
 
-
-
-
-
     return 0;
+}
+
+
+
+
+int buscarIndiceLibreEnArray(char lista[][20],int tamanio,int *pIndice)
+{
+    int i;
+    int retorno=-1;
+
+    for(i=0;i<tamanio;i++)
+    {
+        if(lista[i][0]=='\0')
+        {
+            *pIndice = i;
+            retorno = 0;
+            break;
+        }
+    }
+    return retorno;
 }
