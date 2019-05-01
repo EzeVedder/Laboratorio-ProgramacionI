dfdacc23224bee2243eac74699ecfbe294841158
EzeVedder <pane.ezequiel@gmail.com>
2019-05-01 12:33:46 -0300

    Ejercitando

--- Bibliotecas/biblioteca_funciones/utn_funciones.c
+++ Bibliotecas/biblioteca_funciones/utn_funciones.c
@@ -12,7 +12,7 @@ int utn_getNumber(int* pNumber,char* msg,char*msgError,int min,int max,int retry
     while(retry>0)
     {
         printf("%s",msg);
-        if(scanf("%d",&numero)==1)
+        if(utn_getInt(&numero)==1)//MODIFCO EL SCANF POR LA FUNCION GETINT
         {
             if(numero>=min && numero<=max)
             {
@@ -31,6 +31,40 @@ int utn_getNumber(int* pNumber,char* msg,char*msgError,int min,int max,int retry
     {
         ret=0;
         *pNumber=numero;
+    }
+    return ret;
+}
+
+int utn_getInt(int* pResultado)
+{
+    char buffer[54];
+    int ret=-1;
+    scanf("%s",buffer);
+    if(utn_isNumeric(buffer))
+    {
+        *pResultado = atoi(buffer);
+        ret=1;
+    }
+    return ret;
+}
+
+int utn_isNumeric(char* cadena)
+{
+    int ret=-1;
+    int i=0;
+
+    if(cadena!=NULL)
+    {
+        while(cadena[i]!='\0')
+        {
+            if(cadena[i]<0||cadena[i]>9)
+            {
+                break;
+                i++;
+            }
+        }
+        if(cadena[i]=='\0')
+        ret = 1;
     }
     return ret;
 }
--- Bibliotecas/biblioteca_funciones/utn_funciones.h
+++ Bibliotecas/biblioteca_funciones/utn_funciones.h
@@ -3,6 +3,6 @@
 
 
 int utn_getNumber(int* pNumero,char* msg,char*msgError,int min,int max,int retry);
-
-
+int utn_getInt(int* pResultado);
+int utn_isNumeric(char* cadena);
 #endif // UTN_FUNCIONES_H_INCLUDED
